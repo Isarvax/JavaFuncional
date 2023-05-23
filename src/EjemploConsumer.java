bc9d6f690adfb2e7e4287325fff384b41fe1d236
Isarvae <sblandon2223@cue.edu.com>
2023-05-22 19:03:28 -0500

    cambios

--- src/EjemploConsumer.java
+++ src/EjemploConsumer.java
@@ -4,9 +4,10 @@ import java.util.function.Consumer;
 public class EjemploConsumer {
     public static void main(String[] args) {
         List<String> nombres = new ArrayList<>();
-        nombres.add("Juan");
-        nombres.add("Pedro");
-        nombres.add("Martín");
+        nombres.add("Andres");
+        nombres.add("Santiago");
+        nombres.add("Posada");
+        //asdasdasdasdasdasd
 
         Consumer<String> imprimirEnMayusculas = nombre -> System.out.println(nombre.toUpperCase());
 
